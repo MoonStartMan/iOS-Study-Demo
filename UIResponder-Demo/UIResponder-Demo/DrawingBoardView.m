6af6bd36540e7a0ca36a7720766991cb7d5374b4
MoonStartMan <974556594@qq.com>
2021-03-04 14:07:03 +0800

    优化颜色

--- UIResponder-Demo/UIResponder-Demo/DrawingBoardView.m
+++ UIResponder-Demo/UIResponder-Demo/DrawingBoardView.m
@@ -37,7 +37,7 @@
     //  设置宽度
     CGContextSetLineWidth(context, 5.0);
     //  设置颜色
-    CGContextSetStrokeColorWithColor(context, [[UIColor redColor] CGColor]);
+    CGContextSetStrokeColorWithColor(context, [[UIColor colorWithRed: arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1.0] CGColor]);
     
     for (LineModel *line in self.touchArray) {
         //  开始绘制
